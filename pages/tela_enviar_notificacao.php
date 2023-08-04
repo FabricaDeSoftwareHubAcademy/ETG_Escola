77c82df31f3cd58edae25b6502a6bde6d296d09f
Guilherme Schuindt <guischuindt4@gmail.com>
2023-08-04 14:48:23 -0400

    a

--- pages/tela_enviar_notificacao.php
+++ pages/tela_enviar_notificacao.php
@@ -18,48 +18,48 @@
     include_once("../includes/menu.php")
 
     ?> 
-<div class="tudo">   
- 
-    <div class="pagina">
-        <section class="Enviar-notificacao">
-            <h1>Enviar Notificação</h1>
-        </section>
-        
-        <!--Input-->
+    <div class="tudo">   
+    
+        <div class="pagina">
+            <section class="Enviar-notificacao">
+                <h1>Enviar Notificação</h1>
+            </section>
+            
+            <!--Input-->
 
-        <section class="centralizar-inputs-tela-enviar-notificacao">
+            <section class="centralizar-inputs-tela-enviar-notificacao">
 
-            <div class="input_group field">
-                <input type="input" class="input_field" placeholder="Name" required="">
-                <label for="name" class="input_label">Para:</label> <!--Alterar para o nome do input-->
-            </div>
+                <div class="input_group field">
+                    <input type="input" class="input_field" placeholder="Name" required="">
+                    <label for="name" class="input_label">Para:</label> <!--Alterar para o nome do input-->
+                </div>
 
-            <!--Textarea-->
-            <div class="textarea">
-                <label class="titulo-notificacao" for="story">Notificação:</label>
-                <textarea class="quadrado_text" name="story" rows="5" cols="33"></textarea>
-            </div>
+                <!--Textarea-->
+                <div class="textarea">
+                    <label class="titulo-notificacao" for="story">Notificação:</label>
+                    <textarea class="quadrado_text" name="story" rows="5" cols="33"></textarea>
+                </div>
 
-        </section>
+            </section>
 
-        <!--Botões-->
+            <!--Botões-->
 
-        <section class="centralizar_botoes_enviar_notificacao">
-                
-                <!--Botão Voltar-->
-            <div class="botao-padrao-voltar">
-                <a href="#"><input type="submit" class="botao-voltar-submit"  value="VOLTAR"></a>
-            </div>
+            <section class="centralizar_botoes_enviar_notificacao">
+                    
+                    <!--Botão Voltar-->
+                <div class="botao-padrao-voltar">
+                    <a href="#"><input type="submit" class="botao-voltar-submit"  value="VOLTAR"></a>
+                </div>
 
-                <!--Botão Confirmar-->
-            <div class="botao-padrao-confirmar">
-                <a href="#"><input type="submit" class="botao-confirmar-submit"  value="CONFIRMAR"></a>
-            </div>
+                    <!--Botão Confirmar-->
+                <div class="botao-padrao-confirmar">
+                    <a href="#"><input type="submit" class="botao-confirmar-submit"  value="CONFIRMAR"></a>
+                </div>
 
-        </section>
+            </section>
 
+        </div>
     </div>
-</div>
   
 </body>
 </html>
